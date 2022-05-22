f87ab40cb83d63efd529b3114ed392a91f953063
shiranbruchim <shiran.bruchim@ngsoft.com>
2022-05-22 11:08:16 +0300

    insert Message here

---
+++ src/res/globalStyle.ts
@@ -0,0 +1,40 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyle = StyleSheet.create({
+  SHADOW: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+
+    elevation: 5,
+    borderWidth: 0.001,
+  },
+
+  horizontalScroll: {
+    transform: [{rotateY: '180deg'}],
+  },
+
+  //   Rubik_bold: (fontSize: number) => ({
+  //     fontSize: fontSize,
+  //     fontFamily: Fonts.Rubik_bold,
+  //     color: colors.main_black,
+  //   }),
+
+  //   Rubik_Light: (fontSize) => ({
+  //     fontSize: fontSize,
+  //     fontFamily: Fonts.Rubik_bold,
+  //     color: colors.main_black,
+  //   }),
+
+  //   Rubik_Meduim: (fontSize) => ({
+  //     fontSize: fontSize,
+  //     fontFamily: Fonts,
+  //     color: colors.main_black,
+  //   }),
+});
+
+export default globalStyle;
